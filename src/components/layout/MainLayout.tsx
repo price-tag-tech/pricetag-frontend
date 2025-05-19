efbf8f8339c1db7d9671b6653eafa99121dd4c0f
brave-source <eebiyenrin@gmail.com>
2025-05-19 04:01:13 +0100

    update main layout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
 
-interface MainLayoutProps {
-  // Define your props here
-}
-
-const MainLayout: React.FC<MainLayoutProps> = (props) => {
+const MainLayout: React.FC = () => {
   return (
-    <div>
-      {/* Implement this component based on your Figma design */}
-      <p>MainLayout Component</p>
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 };
