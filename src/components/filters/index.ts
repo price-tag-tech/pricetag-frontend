e1492957eaf3f33719d0147bc67519f1412f8c6a
brave-source <eebiyenrin@gmail.com>
2025-05-20 23:09:30 +0100

    export all sub components for use

---
+++ src/components/filters/index.ts
@@ -0,0 +1,5 @@
+export { default as FilterDropdown } from './FilterDropdown';
+export { default as FilterSection } from './FilterSection';
+export { default as FilterButtonGroup } from './FilterButtonGroup';
+export { default as PriceRangeInput } from './PriceRangeInput';
+export * from './types';
