cbbfa415c5ac5797c23541a08c1715b34b37ecc4
brave-source <eebiyenrin@gmail.com>
2025-05-23 14:46:07 +0100

    email verification component

---
+++ src/pages/EmailVerification.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EmailVerification: React.FC = () => {
+  return (
+    <div>
+      {/* Implement this page based on your Figma design */}
+      <p>RegisterPage</p>
+    </div>
+  );
+};
+
+export default EmailVerification;
