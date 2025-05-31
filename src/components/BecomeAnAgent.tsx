01e38d54d5aa4510a8ee6ff62734d28e364cb52e
Austin Ameh <arewageek@gmail.com>
2025-05-31 04:37:59 +0100

    Refactor BecomeAnAgent component for improved styling and readability; add CallToAction section to HomePage and remove OurFeatures section from home

--- src/components/BecomeAnAgent.tsx
+++ src/components/BecomeAnAgent.tsx
@@ -3,15 +3,15 @@ import Button from './common/Button';
 
 
 type ButtonVariant = "secondary" | "primary" | "outline" | "text";
-type ButtonSize = "sm" | "md" | "lg"; 
+type ButtonSize = "sm" | "md" | "lg";
 
 
 interface BecomeAnAgentProps {
   heading?: string;
   buttonText?: string;
   onButtonClick?: () => void;
-  buttonVariant?: ButtonVariant; 
-  buttonSize?: ButtonSize;      
+  buttonVariant?: ButtonVariant;
+  buttonSize?: ButtonSize;
   bgColor?: string;
 }
 
@@ -19,26 +19,27 @@ const BecomeAnAgent: React.FC<BecomeAnAgentProps> = ({
   heading = "Default Heading Text",
   buttonText = "Click Me",
   onButtonClick,
-  buttonVariant = 'secondary', 
-  buttonSize = 'lg',         
-  bgColor = 'bg-emerald-50' 
+  buttonVariant = 'secondary',
+  buttonSize = 'lg',
+  bgColor
 }) => {
 
   const handleClick = onButtonClick || (() => console.log("Button clicked!"));
 
   return (
-    <div className="py-12 md:py-16 font-['Poppins']">
+    <div className="py-12 md:py-16 font-montserrat">
       <div className="mx-auto px-4 sm:px-6 lg:px-8">
-        <div className={`${bgColor} rounded-2xl shadow-sm text-center py-12 px-6 sm:py-16 sm:px-8 lg:py-20 lg:px-12`}>
-          
-          <h2 className="text-3xl sm:text-4xl lg:text-5xl font-semibold text-slate-800 leading-tight mb-8 md:mb-10 max-w-3xl mx-auto">
+        <div className={`${bgColor ? bgColor : "bg-gradient-to-tl to-black from-[#1B1B1B]"} rounded-2xl shadow py-12 px-6 sm:py-16 sm:px-8 lg:py-24 lg:px-48`}>
+
+          <h2 className="text-3xl sm:text-4xl lg:text-4xl font-normal leading-tight mb-8 md:mb-10 max-w-3xl text-white">
             {heading}
           </h2>
-          
+
           <Button
-            variant={buttonVariant} 
-            size={buttonSize}       
+            variant={buttonVariant}
+            size={buttonSize}
             onClick={handleClick}
+            className='rounded-3xl border-gray-200/70 border-[1.5pt] bg-transparent font-montserrat font-semibold'
           >
             {buttonText}
           </Button>
