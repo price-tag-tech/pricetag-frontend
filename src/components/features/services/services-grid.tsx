a2aa8a996af7996b1465c41ba7dac7a1dbd3d779
brave-source <eebiyenrin@gmail.com>
2025-06-01 11:32:10 +0100

    refactor: modernize ServicesGrid layout with CSS Grid

--- src/components/features/services/services-grid.tsx
+++ src/components/features/services/services-grid.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import ServiceCard, { ServiceCardProps } from './service-card'
+import React from "react";
+import ServiceCard, { ServiceCardProps } from "./service-card";
 
 interface ServicesGridProps {
-    services: ServiceCardProps[],
-    itemsPerRow: number
+  services: ServiceCardProps[];
+  itemsPerRow: number;
 }
 
 const ServicesGrid: React.FC<ServicesGridProps> = (props) => {
-    return (
-        <div className='w-full py-4'>
-            <div className='flex flex-col md:flex-row'>
-                {props.services.map(
-                    (service, index) => <div key={index} className={`w-full md:w-1/${props.itemsPerRow} p-4`}>
-                        <ServiceCard {...service} />
-                    </div>
-                )}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="w-full py-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8">
+        {props.services.map((service, index) => (
+          <div key={service.id || index} className="w-full">
+            <ServiceCard {...service} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default ServicesGrid
+export default ServicesGrid;
