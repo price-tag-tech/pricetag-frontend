ede8cbae2eb5d2f50d8ebc5be9c23ad105d9a964
Austin Ameh <arewageek@gmail.com>
2025-06-07 08:33:52 +0100

    update top-stores - break filter icon to seperate component, create ListOrderAndPurchase component

---
+++ src/components/ui/filter-icon.tsx
@@ -0,0 +1,17 @@
+const FilterIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg
+        width="16"
+        height="10"
+        viewBox="0 0 18 12"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+    >
+        <path
+            d="M7 12H11V10H7V12ZM0 0V2H18V0H0ZM3 7H15V5H3V7Z"
+            fill="currentColor"
+        />
+    </svg>
+);
+
+export default FilterIcon
