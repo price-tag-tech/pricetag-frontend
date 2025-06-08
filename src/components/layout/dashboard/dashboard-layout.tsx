ca04e2b4faa77ba85cc917deb715875f447a9d10
Austin Ameh <arewageek@gmail.com>
2025-06-09 00:06:26 +0100

    Refactor DashboardHeader and UserDashboard components: remove user prop from DashboardHeader, integrate SearchBar, and enhance user profile display with profile menu and referral link.

--- src/components/layout/dashboard/dashboard-layout.tsx
+++ src/components/layout/dashboard/dashboard-layout.tsx
@@ -5,19 +5,12 @@ import Sidenav from './sidenav'
 import DashboardHeader from './dashboard-header'
 
 const DashboardLayout: FC = () => {
-    const user = {
-        name: "Prince Godson",
-        referral: {
-            link: "https://pricetag.ng/c/67d859bc-b2b4"
-        }
-    }
-
     return (
         <div className='min-h-screen flex flex-col overflow-x-hidden font-montserrat'>
             <div className='flex flex-col md:flex-row p-4 md:p-10'>
                 <Sidenav />
                 <main className='px-5 md:px-10 w-full'>
-                    <DashboardHeader {...user} />
+                    <DashboardHeader />
                     <Outlet />
                 </main>
             </div>
--- src/pages/dashboard/user-dashboard.tsx.tsx
+++ src/pages/dashboard/user-dashboard.tsx.tsx
@@ -1,8 +1,86 @@
+import { Copy, Menu, MoreHorizontal, Pen, Store } from 'lucide-react'
 import LatestOrderAndPurchase from "../../components/sections/user-dashboard/latest-order-and-purchases"
+import Button from '../../components/common/Button'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const UserDashboard = () => {
+    const user = {
+        name: "Prince Godson",
+        referral: {
+            link: "https://pricetag.ng/c/67d859bc-b2b4"
+        }
+    }
+
+    const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false)
+
     return (
         <div>
+            <div className='w-full md:px-5 flex items-center justify-between mb-8 md:mb-20'>
+                <div className='flex items-center gap-6'>
+                    <div className='h-10 w-10 md:h-20 md:w-20 rounded-full bg-brand-900 text-white font-semibold text-xl md:text-5xl flex items-center justify-center'>
+                        {user.name.substring(0, 1)}
+                    </div>
+                    <div className='hidden md:flex flex-col gap-4'>
+                        <h4 className='font-bold text-3xl hidden md:block'>
+                            {user.name}
+                        </h4>
+                        <div className='flex gap-x-2'>
+                            <Button to="" variant='outline' size="sm" className='button-outline-secondary'>
+                                <span className='hidden md:block'>
+                                    Edit Profile
+                                </span>
+                                <span className='md:hidden'>
+                                    <Pen className='h-4' />
+                                </span>
+                            </Button>
+
+                            <div className='relative'>
+                                <Button variant='outline' onClick={() => setIsProfileMenuOpen(prev => !prev)} size="sm" to="" className='button-outline-secondary'>
+                                    <MoreHorizontal className='h-4 md:h-auto' />
+                                </Button>
+                                <div className={`${!isProfileMenuOpen && "hidden"} absolute left-0 mt-1 w-44 rounded-xl border bg-white shadow-lg z-10 py-2`}>
+                                    <Link to="/user/profile">
+                                        <button className="w-full text-left px-4 py-2 text-sm text-gray-800 hover:bg-gray-100">
+                                            Edit Account
+                                        </button>
+                                    </Link>
+                                    <Link to="">
+                                        <button className="w-full text-left px-4 py-2 text-sm text-red-500 hover:bg-red-50">
+                                            Delete Account
+                                        </button>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='hidden md:flex gap-x-3'>
+                    <div className='flex gap-2 text-xs items-center bg-gray-100 shadow px-2 py-1.5 rounded-xl'>
+                        <span className='py-2 px-3 rounded-lg h-full flex items-center text-black bg-white font-bold text'>
+                            Referral Link
+                        </span>
+                        <input type="text" disabled value={`${user.referral.link.substring(0, 30)}...`} className='text-gray-600 bg-transparent w-[150pt]' />
+                        <Button variant='secondary' className='text-white px-2'>
+                            <Copy className='h-4' />
+                        </Button>
+                    </div>
+                    <div className='flex'>
+                        <Button variant='outline' size="sm" className='font-montserrat'>
+                            <span className='mr-2'>
+                                <Store />
+                            </span>
+                            My Store
+                        </Button>
+                    </div>
+                </div>
+
+                <div className='md:hidden'>
+                    <Button variant='outline' size='sm' className='button-outline-secondary'>
+                        <Menu />
+                    </Button>
+                </div>
+            </div>
             <LatestOrderAndPurchase />
         </div>
     )
