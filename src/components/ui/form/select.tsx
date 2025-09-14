efa80bc65ff91e0c1964b33c5d5db20cd2a186b8
Austin Ameh <arewageek@gmail.com>
2025-09-14 19:37:19 +0100

    Enhance ui for profile layout and refactor ui form elements

--- src/components/ui/form/select.tsx
+++ src/components/ui/form/select.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react'
+import { getTextSize, TextSize } from '../../../utils/get-text-size'
 
 interface Option {
     label: string,
@@ -8,15 +9,20 @@ interface Option {
 interface SelectProps {
     options?: Option[]
     className?: string
-    default?: string
+    placeholder?: string
     name?: string
-    size?: "xs" | "sm" | "md" | "lg"
+    size?: TextSize
+    change?: (value: string) => void
 }
 
 const Select: FC<SelectProps> = (props) => {
+    const sizeClass = getTextSize(props.size!)
+
     return (
-        <select className={`py-4 p-4 border w-full shadow-sm rounded bg-white text-gray-500 text-${props.size || "sm"} ${props.className}`}>
-            <option className='capitalize'>{props.default ?? `--Select ${props.name?.substring(0, 1).toUpperCase()}${props.name?.substring(1)}--`}</option>
+        <select onChange={(e) => props.change?.(e.target.value)} className={`py-4 p-4 border w-full shadow-sm rounded bg-white text-gray-500 ${sizeClass} ${props.className}`}>
+            <option className='capitalize'>
+                {props.placeholder ?? `--Select ${props.name?.substring(0, 1).toUpperCase()}${props.name?.substring(1)}--`}
+            </option>
             {props.options && props.options.map((option) => <option key={option.value} value={option.value}>
                 {option.label}
             </option>)}
--- src/components/ui/form/textarea.tsx
+++ src/components/ui/form/textarea.tsx
@@ -1,16 +1,28 @@
 import { FC } from 'react'
+import { getTextSize, TextSize } from '../../../utils/get-text-size'
 
 interface TextareaProps {
     className?: string
     name?: string
     placeholder?: string
     value?: string
-    size?: "xs" | "sm" | "md" | "lg"
+    size?: TextSize
+    change?: (value: string) => void
+    rows?: number
 }
 
 const Textarea: FC<TextareaProps> = (props) => {
+
+    const sizeClass = getTextSize(props.size!, 'sm')
+
     return (
-        <textarea value={props.value} placeholder={props.placeholder} className={`py-4 p-4 border shadow-sm rounded w-full text-${props.size || "sm"} ${props.className}`}>
+        <textarea
+            onChange={e => props.change?.(e.target.value)}
+            value={props.value}
+            placeholder={props.placeholder}
+            rows={props.rows || 4}
+            className={`py-4 p-4 border shadow-sm rounded w-full ${sizeClass} ${props.className}`}
+        >
             {props.value && props.value}
         </textarea>
     )
